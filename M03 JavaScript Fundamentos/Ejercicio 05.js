4b43e3127c5badf10a8b72009a6fea1f615e5662
olintolopez <olintolopez@hotmail.com>
2023-11-17 23:41:33 -0400

    Terminado con exito los ejercicios de la Homework 03: Fundamentos de javascript

--- M03 JavaScript Fundamentos/Ejercicio 05.js
+++ M03 JavaScript Fundamentos/Ejercicio 05.js
@@ -69,7 +69,7 @@ function esVocal(letra) {
    // Escribe una función que reciba una letra y, si es una vocal, muestre el mensaje “Es vocal”.
    // Si el usuario ingresa un string de más de un caracter debes retornar el mensaje: "Dato incorrecto".
    // Si no es vocal, tambien debe retornar "Dato incorrecto".
-   // Tu código:
+   // Tu código: 
    var vocales = ["a","e","i","o","u"];
    if (letra.length === 1 && vocales.includes(letra)) 
       return "Es vocal";
